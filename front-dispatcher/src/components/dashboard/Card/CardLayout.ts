48282cffa0ec378f6ea40779a14ef8864a801f65
Omer Nave <>
2022-03-08 15:32:38 +0200

    added responsiveness to card

--- front-dispatcher/src/components/dashboard/Card/CardLayout.ts
+++ front-dispatcher/src/components/dashboard/Card/CardLayout.ts
@@ -7,6 +7,9 @@ const CardLayout = styled.div`
   display: flex;
   box-shadow: 0px 32px 64px rgba(0, 0, 0, 0.05);
   border-radius: 20px;
+  @media only screen and (max-width: 450px) {
+    flex-direction: column;
+  }
 `;
 
 export default CardLayout;
