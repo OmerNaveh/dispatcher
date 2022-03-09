acb6b054379dc1feca0463f9c4f0e4607dc0576c
Omer Nave <>
2022-03-09 16:43:51 +0200

    defined state for clicked on header user button

--- front-dispatcher/src/components/Header/HeaderUserBtn/HeadrUserBtn.tsx
+++ front-dispatcher/src/components/Header/HeaderUserBtn/HeadrUserBtn.tsx
@@ -1,15 +1,15 @@
 import React, { Fragment, useState } from "react";
-import { headerStrings } from "../../../strings/headerString";
 import LogoutBtn from "../../login/LogoutBtn";
 import { StyledHeaderBtn } from "./style";
 interface UserBtnProps {
   userInitials: string;
 }
 export default function HeadrUserBtn({ userInitials }: UserBtnProps) {
-  const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState<boolean>(false);
+
   return (
     <Fragment>
-      <StyledHeaderBtn onClick={() => setClicked((prev) => !prev)}>
+      <StyledHeaderBtn onClick={(e) => setClicked((prev) => !prev)}>
         {userInitials}
       </StyledHeaderBtn>
       {clicked && <LogoutBtn />}
