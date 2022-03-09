79f7fbc764bd9b2909b7d31e3c173a41b9e3b006
Omer Nave <>
2022-03-09 12:46:59 +0200

    userBtn concept drawn

---
+++ front-dispatcher/src/components/Header/HeaderUserBtn/HeadrUserBtn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledHeaderBtn } from "./style";
+
+export default function HeadrUserBtn() {
+  return (
+    <StyledHeaderBtn>
+      {/* insert option element here and an onclick function that used logout from auth0 */}
+    </StyledHeaderBtn>
+  );
+}
---
+++ front-dispatcher/src/components/Header/HeaderUserBtn/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+//import dropdown
+
+// should style dropdown istead
+export const StyledHeaderBtn = styled.button`
+  background: #0058b9;
+  border-radius: 30px;
+`;
