d0232cb78233e999a5ee1ee63d994d874fdbdbd9
Omer Nave <>
2022-03-13 09:11:36 +0200

    removed string and created const in searchBar.tsx

--- front-dispatcher/src/components/SearchBar/SearchBar.tsx
+++ front-dispatcher/src/components/SearchBar/SearchBar.tsx
@@ -4,12 +4,13 @@ import { ReactComponent as SearchIcon } from "../../../assets/search.svg";
 import InputWithIcon from "./InputWithIcon/InputWithIcon";
 import DropDown from "../UI/DropDown/DropDown";
 import { Divider } from "@mui/material";
+import { searchBarStrings } from "../../strings/searchBarStrings";
 const SearchBar = () => {
   return (
     <StyledSearchBarDiv className="grid">
       <InputWithIcon />
       <Divider orientation="vertical" variant="middle" flexItem />
-      <DropDown options={["1"]} />
+      <DropDown options={searchBarStrings.searchDropDownOptions} />
     </StyledSearchBarDiv>
   );
 };
---
+++ front-dispatcher/src/strings/searchBarStrings.ts
@@ -0,0 +1,3 @@
+export const searchBarStrings = {
+  searchDropDownOptions: ["Top Headlines", "Everything"],
+};
