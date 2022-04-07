f793abde7f3f387be6fd30a61466abca33819926
Omer Nave <>
2022-04-07 09:42:21 +0300

    passed users initials correctly

--- front-dispatcher/src/features/Header/components/HeaderUserBtn/HeadrUserBtn.tsx
+++ front-dispatcher/src/features/Header/components/HeaderUserBtn/HeadrUserBtn.tsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React, { Fragment, useState } from "react";
 import LogoutBtn from "../../../LoginPage/components/LogOutBtn/LogoutBtn";
 
@@ -8,11 +9,18 @@ interface UserBtnProps {
 
 const HeadrUserBtn = ({ userInitials }: UserBtnProps) => {
   const [clicked, setClicked] = useState<boolean>(false);
+  const { user } = useAuth0();
 
+  let auth0UserInitials = "";
+  if (user && user.given_name && user.family_name) {
+    auth0UserInitials =
+      user.given_name.slice(0, 1).toUpperCase() +
+      user.family_name.slice(0, 1).toUpperCase();
+  }
   return (
     <Fragment>
       <StyledHeaderBtn onClick={(e) => setClicked((prev) => !prev)}>
-        {userInitials}
+        {auth0UserInitials || userInitials}
       </StyledHeaderBtn>
       {clicked && <LogoutBtn />}
     </Fragment>
