5f0dad29c106301835dc5abe8b4193d86d1ca2e3
Omer Nave <>
2022-04-11 17:02:01 +0300

    created graph data files and routes layout

--- back-dispatcher/index.ts
+++ back-dispatcher/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import topHeadlinesRouter from "./routers/TopHeadlinesRoute/topHeadlinesRoute";
 import everythingRouter from "./routers/EverythingRoute/evertythingRoute";
 import { errorHandler } from "./middlewares/errorHandler";
+import graphsRouter from "./routers/GraphsDataRoute/graphsRoute";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 3003;
 app.use(express.json());
 app.use("/top", topHeadlinesRouter);
 app.use("/everything", everythingRouter);
+app.use("/graph", graphsRouter);
 app.use(errorHandler);
 
 app.listen(port, () => {
