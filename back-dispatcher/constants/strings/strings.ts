e7e304e8cf86cc2107fb65eb728aa43ba31388b6
Omer Nave <>
2022-04-12 12:02:35 +0300

    added adding tags to an article logic

--- back-dispatcher/constants/strings/strings.ts
+++ back-dispatcher/constants/strings/strings.ts
@@ -9,9 +9,76 @@ export const unknownError = "Unknown error";
 export const exceededFreeTierLimit = "Excceded free tier limit of 100 results";
 export const internalServerErrors = "Internal server errors";
 export const tagsKeywords = {
-  Money: ["crypto", "money", "$", "stock", "bitcoin"],
-  Violence: ["gun", "kill", "blood", "threat"],
-  Tech: [],
-  Politics: [],
-  Sport: ["ufc", "nba", "athlete", "sport", "gym", "medal"],
+  Money: [
+    "crypto",
+    "money",
+    "$",
+    "stock",
+    "bitcoin",
+    "כסף",
+    "שקל",
+    "מניה",
+    "מניות",
+  ],
+  Violence: ["gun", "kill", "blood", "threat", "אלימות", "דם", "הרג", "נשק"],
+  Tech: [
+    "tech",
+    "phone",
+    "computer",
+    "gaming",
+    "gamer",
+    "טכנולוגיה",
+    "טלפון",
+    "גיימ",
+    "מחשב",
+  ],
+  Politics: [
+    "election",
+    "vote",
+    "senate",
+    "government",
+    "politic",
+    "פוליטיק",
+    "ממשלה",
+    "בחירות",
+  ],
+  Sport: [
+    "ufc",
+    "nba",
+    "athlete",
+    "sport",
+    "gym",
+    "medal",
+    "ספורט",
+    "מדליה",
+    "אליפות",
+  ],
+  Health: [
+    "health",
+    "medical",
+    "medicine",
+    "cure",
+    "doctor",
+    "בריאות",
+    "רופא",
+    "תרופה",
+    "חיסון",
+  ],
+  Disaster: [
+    "fire",
+    "tsunami",
+    "hurricane",
+    "earthquake",
+    "disaster",
+    "שריפה",
+    "רעידת",
+    "אסון",
+  ],
 };
+export const moneyTag = "Money";
+export const ViolenceTag = "Violence";
+export const techTag = "Tech";
+export const politicsTag = "Politics";
+export const sportTag = "Sport";
+export const healthTag = "Health";
+export const disasterTag = "Disaster";
--- back-dispatcher/utils/addingTagsToArticles.ts
+++ back-dispatcher/utils/addingTagsToArticles.ts
@@ -1,13 +1,42 @@
 import { INewsApiArticle, INewsApiResponse } from "ts-newsapi/lib/types";
+import {
+  disasterTag,
+  healthTag,
+  moneyTag,
+  politicsTag,
+  sportTag,
+  tagsKeywords,
+  techTag,
+  ViolenceTag,
+} from "../constants/strings/strings";
 
 export const addingTagsToArticles = (apiResponse: INewsApiResponse) => {
   if (!apiResponse.articles.length) return apiResponse;
 };
 
 const addTagsToArticle = (article: INewsApiArticle) => {
-  article.description;
+  const { description } = article;
+  const tags = description ? getTagsByString(description) : [];
+  return { ...article, tags };
 };
 
 const getTagsByString = (articleString: string) => {
   const tags: string[] = [];
+  const lowerCaseArticleString = articleString.toLowerCase();
+  if (!articleString) return tags;
+  if (tagsKeywords.Money.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(moneyTag);
+  if (tagsKeywords.Health.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(healthTag);
+  if (tagsKeywords.Disaster.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(disasterTag);
+  if (tagsKeywords.Politics.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(politicsTag);
+  if (tagsKeywords.Sport.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(sportTag);
+  if (tagsKeywords.Tech.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(techTag);
+  if (tagsKeywords.Violence.some((tag) => lowerCaseArticleString.includes(tag)))
+    tags.push(ViolenceTag);
+  return tags;
 };
